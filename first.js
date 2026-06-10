5abfd023bf7618807d67b2438551951570c1ff8b
Sarvesh Kumar <abcode1024@gmail.com>
2026-06-10 19:40:24 +0530

    variable & datatyps and operators

---
+++ first.js
@@ -0,0 +1,44 @@
+// console.log("sarvesh kumar")
+// fullname = "tony stark";
+// age = 24;
+// price = 152.8
+// console.log(fullname)
+// console.log(age)
+// console.log(price)
+
+// x = null;
+// console.log(x)
+// y = undefined
+// console.log(y)
+
+// isfollow = false
+// console.log(isfollow)
+
+// let name = "sarvesh kumar";
+// let age = "25";
+// let price = 7857;
+
+// var age = 65;
+// var age = 78;
+// var age = 86;
+
+// console.log(age);
+
+// let age = 67;
+// age = 89;
+// console.log(age)
+
+// const student = {
+//  name:"abc",
+//  price:45,
+//  ispass:true,
+//  number:100,
+//  age:12
+// }
+
+// student["age"] = student["age"] +10;
+
+// console.log(student["age"]);
+// console.log(student)
+// console.log(student["name"])
+// console.log(student.price)
---
+++ second.js
@@ -0,0 +1,57 @@
+console.log("operators");
+// arithematic operators---------------------------------------------
+
+// let a = "5";
+// let b = 2;
+
+// console.log("a = " + a + " & " + "b = " + b );
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// console.log(a++)
+// console.log(a--)
+
+// assignment operators------------------------------------------------------------------
+
+// a += 4
+// console.log(a)
+// a -= 4
+// console.log(a)
+// a *= 4
+// console.log(a)
+// a /= 4
+// console.log(a)
+// a %= 4
+// console.log(a)
+// a = a**4
+// a**=4
+// console.log(a)
+
+// comparison operators
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a === b);
+// console.log(a !== b);
+// console.log(a !== b);
+
+
+
+// logical opeartors
+
+
+
+let a = 5;
+let b= 6;
+
+let con1 = a!=b;
+let con2 = a<b;
+
+console.log(con1 && con2)
+console.log(con1 || con2)
+console.log(con1 != con2)
