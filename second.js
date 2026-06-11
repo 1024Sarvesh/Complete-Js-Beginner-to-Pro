d9bcb3e5894d85a5257a8ce66dfb249762054c00
Sarvesh Kumar <abcode1024@gmail.com>
2026-06-11 17:17:28 +0530

    conditional staements

--- second.js
+++ second.js
@@ -42,16 +42,136 @@ console.log("operators");
 
 
 
-// logical opeartors
+// ---------------------------------------------------------logical opeartors
 
 
 
-let a = 5;
-let b= 6;
+// let a = 5;
+// let b= 6;
 
-let con1 = a!=b;
-let con2 = a<b;
+// let con1 = a!=b;
+// let con2 = a<b;
 
-console.log(con1 && con2)
-console.log(con1 || con2)
-console.log(con1 != con2)
+// console.log(con1 && con2)
+// console.log(con1 || con2)
+// console.log(con1 != con2)
+
+
+// -----------------------------------------------condinational operators
+
+// ---------------------------------if statement
+
+// const age = 16;
+
+// if(age>18){
+//     console.log("you can vote")
+// }
+// if(age<18){
+//     console.log("you can not vote")
+// }
+
+
+
+// const mode = "dark";
+// let color;
+// if(mode === "dark"){
+//     color = "dark"
+// }
+// if(mode === "light"){
+//     color = "white"
+// }
+// console.log(color)
+
+
+// --------------------------------------if else statement
+
+
+// const mode = "light";
+// let color;
+// if(mode === "dark") {
+//     color = "dark"
+// }else{
+//     color = "white"  
+// }
+
+// console.log(color)
+
+
+// const age = 16;
+
+// if(age>18){
+//     console.log("you can vote")
+// }else{
+//     console.log("you can not vote")
+// }
+
+
+// -------------------------------------odd or even number
+
+
+// const number = 7;
+// if(number%2 === 0){
+//     console.log("number + " + number + " is even")
+// }else{
+//     console.log("number " + number + " is odd")
+
+// }
+
+
+// ---------------------------------- else if statement
+
+// let mode = "dark";
+// let color;
+// if(mode === "dark"){
+//     color = "black"
+// }else if(mode === "pink"){
+//     color = "pink"
+// }else{
+//     color = "white"
+// }
+// console.log(color)
+
+
+// // if we have only one condition then
+// if(mode === "dark") console.log(mode)
+
+
+
+// ----------------------------Ternary operators
+
+
+// let age = 16;
+// // const result = age>18?"adult":"not adult"
+// // console.log(result)
+// age>18? console.log("adult"):console.log("not adult")
+
+
+
+// const Number = prompt("Enter a Number:");
+// console.log(Number)
+// if(Number%5=== 0){
+//     console.log("multiple of 5")
+// }else{
+//     console.log("not the multiple of 5")
+
+// }
+
+
+
+// let scores = prompt("enter the score:")
+// let grade;
+// if(scores>90){
+//     grade = "A grade"
+// }else if(scores>70){
+//     grade = "B grade"
+// }else if(scores>60){
+//    grade = "C grade"
+// }else if(scores>50){
+//     grade = "D grade"
+// }else{
+//     grade = "F grade"
+// }
+// console.log(grade)
+
+
+
