b42cce75f6690f1782753e33842fe760c7383f60
Sarvesh Kumar <abcode1024@gmail.com>
2026-06-22 20:52:42 +0530

    prototype,classes(inherience) part-1

---
+++ Classes&Objects/app.js
@@ -0,0 +1,137 @@
+// const student = {
+//     FullName:"Sarvesh Kumar",
+//     Marks:"95",
+//     PrintMarks:function(){
+//         console.log(`marks = ${this.Marks}`)// this.Marks ka matlab hai ki student.marks
+//     }
+// }
+
+
+
+// ------------------------------make own prototype-----------------------------
+
+// const employee = {
+//     calcTax(){
+//         console.log("Tax rate is 10%")
+//     }
+// }
+
+// const Sarvesh = {
+//     salary:"50000"
+// }
+// const Sarvesh1 = {
+//     salary:"50000"
+// }
+// const Sarvesh2 = {
+//     salary:"50000"
+// }
+// const Sarvesh3 = {
+//     salary:"50000"
+// }
+
+// Sarvesh.__proto__ = employee;
+// Sarvesh1.__proto__ = employee;
+// Sarvesh2.__proto__ = employee;
+// Sarvesh3.__proto__ = employee;
+
+// --------------------------------------- to create object use Classes--------------------
+
+
+// class ToyotaCar{
+//     constructor(brand,milage){
+//         console.log("creating new object")
+//         this.brand = brand;
+//         this.milage = milage;
+//     }
+//     start(){
+//         console.log("start")
+//     }
+//     stop(){
+//         console.log("stop")
+//     }
+
+// }
+
+// const Fortuner = new ToyotaCar("Fortuner",12);
+// console.log(Fortuner)
+// const lexus = new ToyotaCar("Lexus",10);
+// console.log(lexus)
+
+
+
+
+// ----------------------------------------- inheritence--------------------------
+
+
+// class parent{
+//     hello(){
+//         console.log("Hello")
+//     }
+// }
+
+// class child extends parent{}
+// let NewObj = new child()
+
+
+
+
+// class Person{
+//     constructor(){
+//        this.species = "homo sapiens"
+//     }
+//     eat(){
+//         console.log("eat")
+//     }
+//     sleep(){
+//         console.log("sleep")
+//     }
+//     work(){
+//         console.log("Do Nothing")
+//     }
+// }
+
+// class Engineer extends Person{
+//     work(){
+//         console.log("solve problems,built something")
+//     }
+// }
+// class Doctor extends Person{
+//     work(){
+//         console.log("Treat Parents")
+//     }
+// }
+
+// let SarveshObj = new Engineer();
+// let p1 = new Person();
+// let e1 = new Engineer()
+
+
+// -----------------------------------Super Keywords-----------------
+// class Person{
+//     constructor(){
+//        this.species = "homo sapiens"
+//        console.log("Enter Parent Contructor")
+//     }
+//     eat(){
+//         console.log("eat")
+//     }
+// }
+
+
+// class Engineer extends Person{
+
+//     constructor(branch){
+//         console.log("Enter Child Contructor")
+//         super();   //to call the parent class constructor
+//         this.branch = branch;
+//         console.log("Exit Child Contructor")
+//     }
+
+//     work(){
+//         console.log("solve problems,built something")
+//     }
+// }
+
+// let engObj = new Engineer("chemical engineer");
+
+// console.log(engObj)
